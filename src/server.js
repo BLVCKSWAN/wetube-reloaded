7af17f7d017fb6085dd13504706e953aa46e3ade
BLVCKSWAN <blvckswanbegins@gmail.com>
2021-12-22 14:21:08 +0900

    211222

--- src/server.js
+++ src/server.js
@@ -5,19 +5,23 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res, next) => {
-  console.log("I will respond.");
-  return res.send("Hello");
-};
-
-const login = (req, res) => {
-  return res.send("login");
-};
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server Listening on port http://localhost:${PORT}`);
