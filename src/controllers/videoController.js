f0697cad774aa11ad3b182c9c11644d61e232db5
blvckswan <93452543+BLVCKSWAN@users.noreply.github.com>
2021-12-25 16:52:11 +0900

    #5.5

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,8 @@
-export const trending = (req, res) => res.send("Home page Videos");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const search = (req, res) => res.send("Search");
 
-export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  return res.send("Edit");
-};
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
   return res.send("Delete Video");
