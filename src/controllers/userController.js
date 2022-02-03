9b2a0825da6608670a1efb64eb1ae3c547bfa1d8
blvckswan <93452543+BLVCKSWAN@users.noreply.github.com>
2022-02-04 07:27:27 +0900

    #7.1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,19 @@
-export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Log in");
+import User from "../models/User";
 
-export const edit = (req, res) => res.send("Edit user");
-export const remove = (req, res) => res.send("Delete user");
-export const logout = (req, res) => res.send("Log out");
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, location } = req.body;
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  res.redirect("/login");
+};
+export const login = (req, res) => res.send("Login");
+export const edit = (req, res) => res.send("Edit User");
+export const remove = (req, res) => res.send("Delete User");
+export const logout = (req, res) => res.send("Logout");
 export const see = (req, res) => res.send("See User");
